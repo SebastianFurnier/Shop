fe21ae24775c5788aba484d468d9d6ac4de5c493
SebastianFurnier <sfurnier@hotmail.com>
2024-12-15 20:46:37 -0300

    add JUnit

--- src/main/java/com/project/shop/Controller/SellController.java
+++ src/main/java/com/project/shop/Controller/SellController.java
@@ -2,10 +2,14 @@ package com.project.shop.Controller;
 
 import com.project.shop.Service.ISellService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/sell")
 public class SellController {
     @Autowired
     private ISellService sellService;
+
+
 }
