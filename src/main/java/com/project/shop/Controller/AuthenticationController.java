cb1c82d5c46bfe72dbaa9752961b4c08e1f502fc
SebastianFurnier <sfurnier@hotmail.com>
2024-12-23 23:06:35 -0300

    JWT implementation

---
+++ src/main/java/com/project/shop/Controller/AuthenticationController.java
@@ -0,0 +1,26 @@
+package com.project.shop.Controller;
+
+import com.project.shop.DTO.AuthLoginRequestDTO;
+import com.project.shop.DTO.AuthResponseDTO;
+import com.project.shop.Service.Imp.UserDetailsServices;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+    @Autowired
+    private UserDetailsServices userDetailsServices;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody @Valid AuthLoginRequestDTO userRequest) {
+        return new ResponseEntity<>(userDetailsServices.loginUser(userRequest), HttpStatus.OK);
+    }
+}
